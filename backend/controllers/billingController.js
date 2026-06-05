f4eef54f7d4915798ad9cffb0c29706939425458
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-06-05 16:32:06 +0530

    Merge remote changes

--- backend/controllers/billingController.js
+++ backend/controllers/billingController.js
@@ -42,34 +42,3 @@ exports.downloadInvoice = async (req, res) => {
     });
   }
 };
-exports.downloadInvoice = async (req, res) => {
-  try {
-    const Invoice = require("../models/Invoice");
-    const generateInvoice = require("../utils/generateInvoice");
-
-    const invoice = await Invoice.findById(req.params.id)
-      .populate("customer");
-
-    if (!invoice) {
-      return res.status(404).json({
-        success: false,
-        message: "Invoice not found",
-      });
-    }
-
-    const pdfBuffer = await generateInvoice(invoice);
-
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader(
-      "Content-Disposition",
-      `attachment; filename=${invoice.invoiceNo}.pdf`
-    );
-
-    res.send(pdfBuffer);
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
