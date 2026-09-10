08abd03e0530c7d0eace46cbd75e1f953cb7ebff
Souradipta Patra <patrasouradipta3028@gmail.com>
2026-09-11 02:05:38 +0530

    CustomerDashboard

--- frontend/src/pages/customer/MyOrders.jsx
+++ frontend/src/pages/customer/MyOrders.jsx
@@ -18,7 +18,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 import api from "../../api/api";
-import "./CustomerOrders.css";
+import "./MyOrders.css";
 
 const STATUS_CONFIG = {
   draft: {
